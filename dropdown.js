081d11ea9e209d7b9c132f76209a1a0f3310934b
Gianluca Andrisani <andrisanigianlucaux@gmail.com>
2024-06-16 21:12:58 +0100

    slightly better dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,9 +1,24 @@
 const dropdown = document.querySelector('.dropdown');
+let timeout;
 
 dropdown.addEventListener('mouseenter', ()=> {
+    clearTimeout(timeout);
     dropdown.classList.add('show');
 })
 
 dropdown.addEventListener('mouseleave', ()=> {
-    dropdown.classList.remove('show');
+    timeout = setTimeout (()=> {
+        dropdown.classList.remove('show');
+    }, 200);
 })
+
+const options = document.querySelector('.options');
+options.addEventListener('mouseenter',() => {
+    clearTimeout(timeout);
+});
+
+options.addEventListener('mouseleave', () => {
+    timeout = setTimeout(() => {
+        dropdown.classList.remove('show');
+    }, 200); 
+});

5e4279f3a80b70d863f3bbdfee12718598468cd9
Gianluca Andrisani <andrisanigianlucaux@gmail.com>
2024-06-16 20:56:12 +0100

    added behaviour to the drodown

---
+++ dropdown.js
@@ -0,0 +1,9 @@
+const dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('mouseenter', ()=> {
+    dropdown.classList.add('show');
+})
+
+dropdown.addEventListener('mouseleave', ()=> {
+    dropdown.classList.remove('show');
+})
